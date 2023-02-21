312b0b31ef6f17946cb0796928ef9c62ecef9de6
erivaldo junior <erivaldo.junior@referenceit.com.br>
2023-02-21 11:37:42 -0300

    novos npcs

---
+++ data/npc/scripts/potions_rings_trade.lua
@@ -0,0 +1,64 @@
+local keywordHandler = KeywordHandler:new()
+local npcHandler = NpcHandler:new(keywordHandler)
+NpcSystem.parseParameters(npcHandler)
+
+function onCreatureAppear(cid)              npcHandler:onCreatureAppear(cid)            end
+function onCreatureDisappear(cid)           npcHandler:onCreatureDisappear(cid)         end
+function onCreatureSay(cid, type, msg)      npcHandler:onCreatureSay(cid, type, msg)    end
+function onThink()                          npcHandler:onThink()                        end
+
+local voices = { {text = "Runes, wands, rods, health and mana potions! Have a look!"} }
+npcHandler:addModule(VoiceModule:new(voices))
+
+local shopModule = ShopModule:new()
+npcHandler:addModule(shopModule)
+
+keywordHandler:addKeyword({'stuff'}, StdModule.say, {npcHandler = npcHandler, text = 'Just ask me for a {trade} to see my offers.'})
+keywordHandler:addAliasKeyword({'wares'})
+keywordHandler:addAliasKeyword({'offer'})
+
+shopModule:addBuyableItem({'small health'}, 8704, 20, 1, 'small health potion')
+shopModule:addBuyableItem({'health potion'}, 7618, 45, 1, 'health potion')
+shopModule:addBuyableItem({'mana potion'}, 7620, 50, 1, 'mana potion')
+shopModule:addBuyableItem({'strong health'}, 7588, 100, 1, 'strong health potion')
+shopModule:addBuyableItem({'strong mana'}, 7589, 80, 1, 'strong mana potion')
+shopModule:addBuyableItem({'great health'}, 7591, 190, 1, 'great health potion')
+shopModule:addBuyableItem({'great mana'}, 7590, 120, 1, 'great mana potion')
+shopModule:addBuyableItem({'great spirit'}, 8472, 190, 1, 'great spirit potion')
+shopModule:addBuyableItem({'ultimate health'}, 8473, 310, 1, 'ultimate health potion')
+shopModule:addBuyableItem({'antidote potion'}, 8474, 50, 1, 'antidote potion')
+
+shopModule:addBuyableItem({'stealth ring'}, 2165, 5000, 1, 'stealth ring')
+shopModule:addBuyableItem({'energy ring'}, 2167, 5000, 1, 'energy ring')
+shopModule:addBuyableItem({'life ring'}, 2168, 5000, 1, 'life ring')
+shopModule:addBuyableItem({'dwarven ring'}, 2213, 5000, 1, 'dwarven ring')
+shopModule:addBuyableItem({'ring of healing'}, 2214, 5000, 1, 'ring of healing')
+
+
+shopModule:addSellableItem({'normal potion flask', 'normal flask'}, 7636, 5, 'empty small potion flask')
+shopModule:addSellableItem({'strong potion flask', 'strong flask'}, 7634, 10, 'empty strong potion flask')
+shopModule:addSellableItem({'great potion flask', 'great flask'}, 7635, 15, 'empty great potion flask')
+
+shopModule:addBuyableItemContainer({'bp ap'}, 2002, 8378, 2000, 1, 'backpack of antidote potions')
+shopModule:addBuyableItemContainer({'bp slhp'}, 2000, 8610, 400, 1, 'backpack of small health potions')
+shopModule:addBuyableItemContainer({'bp hp'}, 2000, 7618, 900, 1, 'backpack of health potions')
+shopModule:addBuyableItemContainer({'bp mp'}, 2001, 7620, 1000, 1, 'backpack of mana potions')
+shopModule:addBuyableItemContainer({'bp shp'}, 2000, 7588, 2000, 1, 'backpack of strong health potions')
+shopModule:addBuyableItemContainer({'bp smp'}, 2001, 7589, 1600, 1, 'backpack of strong mana potions')
+shopModule:addBuyableItemContainer({'bp ghp'}, 2000, 7591, 3800, 1, 'backpack of great health potions')
+shopModule:addBuyableItemContainer({'bp gmp'}, 2001, 7590, 2400, 1, 'backpack of great mana potions')
+shopModule:addBuyableItemContainer({'bp gsp'}, 1999, 8376, 3800, 1, 'backpack of great spirit potions')
+shopModule:addBuyableItemContainer({'bp uhp'}, 2000, 8377, 6200, 1, 'backpack of ultimate health potions')
+
+
+
+function creatureSayCallback(cid, type, msg)
+	if not npcHandler:isFocused(cid) then
+		return false
+	end
+
+	return true
+end
+
+npcHandler:setCallback(CALLBACK_MESSAGE_DEFAULT, creatureSayCallback)
+npcHandler:addModule(FocusModule:new())
