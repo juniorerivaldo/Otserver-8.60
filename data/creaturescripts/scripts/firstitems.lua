5fa432e05cc928cc954805129173667ad4872d85
juniorerivaldo <esttstart@gmail.com>
2024-01-17 17:49:51 -0300

    nova versão do first itens

--- data/creaturescripts/scripts/firstitems.lua
+++ data/creaturescripts/scripts/firstitems.lua
@@ -1,72 +1,69 @@
 local firstItems = {}
 firstItems[0] =
 {
-2173,
+2383,
 2525,
-3965,
-2124,
-2457,
 2647,
-2643
+2173,
+2457,
+2463,
+2643  
 }
 firstItems[1] =
 {
-2173,
+2190,
 2525,
-8922,
-2124,
-2457,
 2647,
-2643
+2173,
+2457,
+2463,
+2643  
 }
 firstItems[2] =
 {
-2173,
+2182,
 2525,
-8922,
-2124,
-2457,
 2647,
-2643
+2173,
+2457,
+2463,
+2643  
 }
 firstItems[3] =
 {
-2173,
+2389,
 2525,
-8851,
-2543,
-2124,
-2457,
 2647,
-2643
+2173,
+2457,
+2463,
+2643  
 }
 firstItems[4] =
 {
-2173,
+2383,
 2525,
-8926,
-7403,
-2124,
-2457,
 2647,
-2643
+2173,
+2457,
+2463,
+2643  
 }
-
 function onLogin(cid)
 if getPlayerStorageValue(cid, 30001) == -1 then
 for i = 1, table.maxn(firstItems[getPlayerVocation(cid)]) do
-doPlayerAddItem(cid, firstItems[getPlayerVocation(cid)], 1)
+doPlayerAddItem(cid, firstItems[getPlayerVocation(cid)][i], 1)
 end
 if getPlayerSex(cid) == 0 then
-doPlayerAddItem(cid, 2463, 1)
+doPlayerAddItem(cid, 2554, 1)
 else
-doPlayerAddItem(cid, 2463, 1)
+doPlayerAddItem(cid, 2120, 1)
 end
-local bag = doPlayerAddItem(cid, 1988, 1)
-doAddContainerItem(bag, 2160, 4)
-doAddContainerItem(bag, 2554, 1)
-doAddContainerItem(bag, 2120, 1)
+local bag = doPlayerAddItem(cid, 10518, 1)
+doAddContainerItem(bag, 7620, 1)
 doAddContainerItem(bag, 7618, 1)
+doAddContainerItem(bag, 2160, 3)
+doAddContainerItem(bag, 2554, 1)
 setPlayerStorageValue(cid, 30001, 1)
 end
 return TRUE
