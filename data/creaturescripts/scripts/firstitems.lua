f025dd06850e39adbf172b0f6e214d00dcc3e43b
juniorerivaldo <esttstart@gmail.com>
2024-01-18 08:01:48 -0300

    deixando corda dentro da bag no first itens

--- data/creaturescripts/scripts/firstitems.lua
+++ data/creaturescripts/scripts/firstitems.lua
@@ -54,16 +54,12 @@ if getPlayerStorageValue(cid, 30001) == -1 then
 for i = 1, table.maxn(firstItems[getPlayerVocation(cid)]) do
 doPlayerAddItem(cid, firstItems[getPlayerVocation(cid)][i], 1)
 end
-if getPlayerSex(cid) == 0 then
-doPlayerAddItem(cid, 2554, 1)
-else
-doPlayerAddItem(cid, 2120, 1)
-end
 local bag = doPlayerAddItem(cid, 10518, 1)
 doAddContainerItem(bag, 7620, 1)
 doAddContainerItem(bag, 7618, 1)
 doAddContainerItem(bag, 2160, 3)
 doAddContainerItem(bag, 2554, 1)
+doAddContainerItem(bag, 2120, 1)
 setPlayerStorageValue(cid, 30001, 1)
 end
 return TRUE
