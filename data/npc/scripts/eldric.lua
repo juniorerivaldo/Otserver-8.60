234b7d5538cf7393dd2511b2914a6623588540ba
juniorerivaldo <esttstart@gmail.com>
2024-01-28 11:56:39 -0300

    teste

--- data/npc/scripts/eldric.lua
+++ data/npc/scripts/eldric.lua
@@ -19,13 +19,11 @@ local function creatureSayCallback(cid, type, msg)
                 'preciso de sua ajuda para resgatar minhas notas que foram roubadas pelo mago!'
             }, cid)
             npcHandler.topic[cid] = 2
-        elseif player:getStorageValue(PlayerStorageKeys.Quests.notasDeEldric) ==
-            1 then
+        elseif player:getStorageValue(PlayerStorageKeys.Quests.notasDeEldric) == 1 then
             npcHandler:say(
                 'va até o subsolo e encontre o covil do mago, e me traga a minha nota que esta escondida la.',
                 cid)
-        elseif player:getStorageValue(PlayerStorageKeys.Quests.notasDeEldric) ==
-            2 then
+        elseif player:getStorageValue(PlayerStorageKeys.Quests.notasDeEldric) == 2 then
             local itemId = {2150, 2149, 2147, 2146}
             for i = 1, #itemId do player:addItem(itemId[i], 1) end
             player:setStorageValue(PlayerStorageKeys.Quests.notasDeEldric, 3)
@@ -35,14 +33,15 @@ local function creatureSayCallback(cid, type, msg)
         else
             npcHandler:say('volte com minha nota o mais rapido possivel.', cid)
         end
-        if npcHandler.topic[cid] == 2 then
-            player:addItem(2087, 1)
-            player:setStorageValue(PlayerStorageKeys.Quests.notasDeEldric, 1)
-            npcHandler:say({
-                'PEGUE esta chave para acessar os aposentos do mago .'
-            }, cid)
-            npcHandler.topic[cid] = 0
-        end
+
+        -- Move this block outside the condition
+        player:addItem(2087, 1)
+        player:setStorageValue(PlayerStorageKeys.Quests.notasDeEldric, 1)
+        npcHandler:say({
+            'PEGUE esta chave para acessar os aposentos do mago.'
+        }, cid)
+        npcHandler.topic[cid] = 0
+
     else
         npcHandler:say(
             'Then the downfall of Carlin is inescapable. Please think about it. You know where to find me.',
