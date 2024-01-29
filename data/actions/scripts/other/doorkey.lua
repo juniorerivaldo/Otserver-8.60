68e14ae47de3f533384b6e1e15f1155dc60a3d30
juniorerivaldo <esttstart@gmail.com>
2024-01-29 17:46:01 -0300

    teste d

--- data/actions/scripts/other/doorkey.lua
+++ data/actions/scripts/other/doorkey.lua
@@ -1,17 +1,24 @@
 function onUse(cid, item, frompos, item2, topos)
     portafechada = 5105 -- coloque aqui o id da porta fechada
+    
     portaaberta = 5106 -- coloque aqui o id da porta aberta
+    
     idkey = 2087 -- coloque aqui o id da key
-
-    if item.itemid == idkey then
-        if item2.itemid == portafechada then
-            addEvent(doTransformItem, 2000, item2.uid, portaaberta) -- Aguarda 2000 milissegundos (2 segundos) antes de transformar a porta
-        elseif item2.itemid == portaaberta then
-            addEvent(doTransformItem, 2000, item2.uid, portafechada) -- Aguarda 2000 milissegundos (2 segundos) antes de transformar a porta
-        else
-            doPlayerSendTextMessage(cid, 22, "Sorry, not possible.")
-        end
+    
+       if item.itemid == idkey and item2.itemid == portafechada then
+    
+          doTransformItem(item2.uid,portaaberta)
+    
+       elseif item.itemid == idkey and item2.itemid == portaaberta then
+    
+          doTransformItem(item2.uid,portafechada)
+    
+       else
+    
+          doPlayerSendTextMessage(cid,22,"Sorry, not possible.")
+    
+       end
+    
+       return 1
+    
     end
-
-    return 1
-end
