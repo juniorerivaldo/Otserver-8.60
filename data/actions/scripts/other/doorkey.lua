4e4ac656a10d865373946448719c1e639aec9f3c
juniorerivaldo <esttstart@gmail.com>
2024-01-29 17:12:54 -0300

    teste close door

--- data/actions/scripts/other/doorkey.lua
+++ data/actions/scripts/other/doorkey.lua
@@ -1,8 +1,8 @@
 function onUse(cid, item, frompos, item2, topos)
 
-    portafechada = 5101 -- coloque aqui o id da porta fechada
+    portafechada = 5105 -- coloque aqui o id da porta fechada
 
-    portaaberta = 5102 -- coloque aqui o id da porta aberta
+    portaaberta = 5106 -- coloque aqui o id da porta aberta
 
     idkey = 2087 -- coloque aqui o id da key
 
