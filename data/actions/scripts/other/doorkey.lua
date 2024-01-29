c4b43ef0fe584b7f8bdc9b227d94ed7f074004a2
juniorerivaldo <esttstart@gmail.com>
2024-01-29 17:56:04 -0300

    ahhhrg

--- data/actions/scripts/other/doorkey.lua
+++ data/actions/scripts/other/doorkey.lua
@@ -1,6 +1,6 @@
 function onUse(cid, item, frompos, item2, topos)
-    portafechada = 5101 -- coloque aqui o id da porta fechada
-    portaaberta = 5102 -- coloque aqui o id da porta aberta
+    portafechada = 5099 -- coloque aqui o id da porta fechada
+    portaaberta = 5100 -- coloque aqui o id da porta aberta
     idkey = 2087 -- coloque aqui o id da key
     
     if item.itemid == idkey then
