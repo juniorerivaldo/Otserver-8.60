dd796e99c682229eee69746e798cf120d43c21c7
juniorerivaldo <esttstart@gmail.com>
2024-01-30 22:43:48 -0300

    Corrigido eldric

--- data/actions/scripts/quests/eldric_notes.lua
+++ data/actions/scripts/quests/eldric_notes.lua
@@ -1,7 +1,7 @@
 function onUse(player, item, fromPosition, target, toPosition, isHotkey)
     if player:getStorageValue(45001) == 0 then
         player:setStorageValue(45001, 1)
-        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'PARTE 2 OK')
+        player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'Volte ao templo para falar com Eldric')
     end
 
 end
--- data/npc/scripts/eldric.lua
+++ data/npc/scripts/eldric.lua
@@ -12,57 +12,58 @@ local storage_1 = 45001
 local function greetCallback(cid)
     local player = Player(cid)
     local storage = player:getStorageValue(storage_1)
+
     if storage < 0 then
-        npcHandler:setMessage(MESSAGE_GREET, "Pode me ajudar com uma {quest}?")
+        npcHandler:setMessage(MESSAGE_GREET, "Ola novato quer fazer uma {quest}? preciso de sua ajuda para recuperar um item...")
         return true
     elseif storage < 1 then
-        npcHandler:setMessage(MESSAGE_GREET, "Have you found her?")
+        npcHandler:setMessage(MESSAGE_GREET, "Ja encontrou minhas notas?")
         return true
     elseif storage == 1 then
-        npcHandler:setMessage(MESSAGE_GREET, "Thank you for finding Rihanna!")
+        npcHandler:setMessage(MESSAGE_GREET, "Ohh, sabia que poderia contar com voce!")
         return true
     elseif storage == 2 then
-        npcHandler:setMessage(MESSAGE_GREET, "ja completou cai fora..")
+        npcHandler:setMessage(MESSAGE_GREET, "Bom dia caro aventureiro, como voce esta hoje?")
         return true
     end
+
 end
 
 local function creatureSayCallback(cid, type, msg)
     if not npcHandler:isFocused(cid) then return false end
-
     local player = Player(cid)
     local storage = player:getStorageValue(storage_1)
 
     -- starting quest
     if msgcontains(msg, "quest") and storage < 0 then
-        npcHandler:say("colete o item na caverna {yes}", cid)
+        npcHandler:say("Preciso que voce va até o subsolo da cidade para recuperar minhas notas, elas foram roubadas por algum mago! voce vai ? {sim}", cid)
         npcHandler.topic[cid] = 1
 
         -- starting quest yes/no reply from player
     elseif npcHandler.topic[cid] == 1 then
-        if msgcontains(msg, "yes") then
+        if msgcontains(msg, "sim") then
             player:setStorageValue(storage_1, 0)
-            npcHandler:say(
-                "I will be waiting for you! I hope you can find her..", cid)
+            npcHandler:say("Pegue essa chave para abrir a porta do covil, depois me devolva ela.", cid)
+            player:addItem(2087,1)
+            player:sendTextMessage(MESSAGE_EVENT_ADVANCE, 'Voce recebeu a chave do covil!')
         else
-            npcHandler:say("Very well then.", cid)
+            npcHandler:say("Muito bem.", cid)
         end
         npcHandler.topic[cid] = 0
 
         -- if player has started but not finished quest yet
     elseif storage > 0 and storage < 3 then
-        if msgcontains(msg, "yes") then
+        if msgcontains(msg, "sim") then
             if storage == 1 then
                 player:addItem(2647, 1)
                 player:addItem(2463, 1)
                 player:setStorageValue(storage_1, 2)
+                player:removeItem(2087,1)
+                player:removeItem(1948,1)
                 player:getPosition():sendMagicEffect(CONST_ME_MAGIC_BLUE)
-                npcHandler:say(
-                    "Thank you for finding her! Now I need to put my moves in.. Here, take this a reward!",
-                    cid)
+                npcHandler:say("Voce merece essa recompensa", cid)
             elseif storage == 2 then
-                npcHandler:say("completou ja não tem mais nada pra fazer aqui",
-                               cid)
+                npcHandler:say("Bom dia caro aventureiro, como voce esta hoje?", cid)
             end
         end
 
